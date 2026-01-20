ecafba0e8684afcc365acad4e968140fd269913a
JoaoVictorTeixeira <jxvxteixeira777@outlook.com>
2026-01-20 15:34:48 -0300

    feat: setup initial infra with mysql, mongodb and application properties

---
+++ spacex-batch/src/main/java/com/joaodev/spacex_batch/SpacexBatchApplication.java
@@ -0,0 +1,13 @@
+package com.joaodev.spacex_batch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpacexBatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpacexBatchApplication.class, args);
+	}
+
+}
---
+++ spacex-batch/src/test/java/com/joaodev/spacex_batch/SpacexBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joaodev.spacex_batch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpacexBatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
