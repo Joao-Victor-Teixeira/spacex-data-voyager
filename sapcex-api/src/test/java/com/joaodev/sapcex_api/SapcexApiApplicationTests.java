dc0b0ddb90a7dd8d06296c73262ef1ceddfbbf14
JoaoVictorTeixeira <jxvxteixeira777@outlook.com>
2026-01-20 19:04:23 -0300

    creation of the SpaceX API

---
+++ sapcex-api/src/test/java/com/joaodev/sapcex_api/SapcexApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joaodev.sapcex_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SapcexApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
