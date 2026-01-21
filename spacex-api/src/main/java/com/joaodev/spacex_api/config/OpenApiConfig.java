22c06de5c09a7a127d5df2e755458d3ef33699df
JoaoVictorTeixeira <jxvxteixeira777@outlook.com>
2026-01-21 15:10:05 -0300

    add OpenAPI Swagger, OpenApiConfig

---
+++ spacex-api/src/main/java/com/joaodev/spacex_api/config/OpenApiConfig.java
@@ -0,0 +1,26 @@
+package com.joaodev.spacex_api.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+
+@OpenAPIDefinition
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI dsmovieAPI() {
+        return new OpenAPI()
+           .info(new Info()
+           .title("Space data Voyager")
+           .description("Aplicação backend em Spring Batch que automatiza o consumo da SpaceX API.")
+           .version("v0.0.1")
+           .license(new License()
+           .name("MIT license")
+           .url("https://github.com/Joao-Victor-Teixeira/spacex-data-voyager")));
+    }
+}
