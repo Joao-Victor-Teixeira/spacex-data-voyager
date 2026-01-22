93203103ba9ae8b20bff742eeccc4076d1dce8bb
JoaoVictorTeixeira <jxvxteixeira777@outlook.com>
2026-01-22 15:03:15 -0300

    feat: finalize launch batch job with successful mongodb persistence

--- spacex-launches-batch/src/main/java/com/joaodev/spacex_launches_batch/job/JobConfig.java
+++ spacex-launches-batch/src/main/java/com/joaodev/spacex_launches_batch/job/JobConfig.java
@@ -7,6 +7,7 @@ import org.springframework.batch.core.Step;
 import org.springframework.batch.core.job.builder.JobBuilder;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
@@ -14,7 +15,8 @@ public class JobConfig {
 
     private static Logger logger = LoggerFactory.getLogger(JobConfig.class);
 
-    public Job job(JobRepository jobRepository, Step fetchLaunchDataAndStoreInDatabase){
+    @Bean
+    public Job jobLaunch(JobRepository jobRepository, Step fetchLaunchDataAndStoreInDatabase){
 
         logger.info("INICIANDO TRABALHO DE EXECUÇÃO ...");
 
