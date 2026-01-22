e26a231d7ac4a3b492fb80f269238ac70eb3fac9
JoaoVictorTeixeira <jxvxteixeira777@outlook.com>
2026-01-22 10:51:25 -0300

    create launch batch project

---
+++ spacex-launches-batch/src/main/java/com/joaodev/spacex_launches_batch/SpacexLaunchesBatchApplication.java
@@ -0,0 +1,13 @@
+package com.joaodev.spacex_launches_batch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpacexLaunchesBatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpacexLaunchesBatchApplication.class, args);
+	}
+
+}
---
+++ spacex-launches-batch/src/test/java/com/joaodev/spacex_launches_batch/SpacexLaunchesBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joaodev.spacex_launches_batch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpacexLaunchesBatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
