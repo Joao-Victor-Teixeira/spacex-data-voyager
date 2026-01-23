fa9717f87205f480b7b7b7623a33e16837f17fbe
JoaoVictorTeixeira <jxvxteixeira777@outlook.com>
2026-01-23 18:55:27 -0300

    Create mission batch project

---
+++ spacex-missions-batch/src/main/java/com/joaodev/spacex_missions_batch/SpacexMissionsBatchApplication.java
@@ -0,0 +1,13 @@
+package com.joaodev.spacex_missions_batch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpacexMissionsBatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpacexMissionsBatchApplication.class, args);
+	}
+
+}
